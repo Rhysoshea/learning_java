056e2eaf8b6562e50fe94f92e471dc7097c2af73
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-09-06 21:13:32 +0900

    added ternary operators

--- Operators/src/App.java
+++ Operators/src/App.java
@@ -59,12 +59,43 @@ public class App {
         // || is logical OR operator, operates on boolean
 
         boolean isCar = false;
-        if (isCar = true) { // even though this isn't the correct ==, this is working because it returns a boolean when isCar is being assigned to true
-            System.out.println("This is not supposed to happen");
-        }
+        // if (isCar = true) { // even though this isn't the correct ==, this is working because it returns a boolean when isCar is being assigned to true
+        //     System.out.println("This is not supposed to happen");
+        // }
         if (!isCar) { // ! is a logical compliment operator and tests the alternative value of the boolean
             System.out.println("This is not a car");
         }
+
+        // ternary operator
+        // is a shortcut to assigning one of two values to a variable depending on a condition
+        // eg. int ageOfClient = 20
+        // boolean isEighteenOrOver = (ageOfClient == 20) ? true : false;
+        // isEighteenOrOver becomes true
+        // was Car becomes true if isCar is true, otherwise it will be assigned false
+        boolean wasCar = isCar ? true : false;
+        if (wasCar) {
+            System.out.println("was Car is true");
+        }
+
+        // operator challenge
+        // create a double with value 20.00
+        // create a variable of double with val 80.00
+        // add both variables together and multiply by 100
+        // get the remainder of result with 40
+        // create a bool that assigns true if remainder is 0
+        // output the boolean
+        // write an if then statement that displays "got some remainder" if boolean is not true
+
+        double firstVal = 20.00;
+        double secondVal = 80.00;
+        double thirdValue = (firstVal + secondVal ) * 100.00;
+        double remain = thirdValue % 40.00;
+        System.err.println("remainder: " + remain);
+        boolean noRemain = (remain == 0) ? true : false;
+        if (!noRemain) {
+            System.err.println("Got some remainder");
+        }
+
 
     }
 }
