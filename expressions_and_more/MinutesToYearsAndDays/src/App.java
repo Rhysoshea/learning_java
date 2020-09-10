f50dbed44f8549996bc358ba1771a432bdcfd94d
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-09-10 18:05:04 +0900

    exercise 9

---
+++ expressions_and_more/MinutesToYearsAndDays/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        MinutesToYearsDaysCalculator.printYearsAndDays(0);
+        MinutesToYearsDaysCalculator.printYearsAndDays(525600);
+        MinutesToYearsDaysCalculator.printYearsAndDays(1051200);
+        MinutesToYearsDaysCalculator.printYearsAndDays(561600);
+
+    }
+}
