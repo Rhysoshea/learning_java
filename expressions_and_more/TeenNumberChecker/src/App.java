1441ceaf9c59c45a259ac599e00c1d4e840940e1
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-09-10 14:38:32 +0900

    exercise 7

---
+++ expressions_and_more/TeenNumberChecker/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+        System.out.println(TeenNumberChecker.hasTeen(9, 99, 19));
+        System.out.println(TeenNumberChecker.hasTeen(-1, 0, -100));
+        System.out.println(TeenNumberChecker.hasTeen(13, 14, 15));
+        System.out.println(TeenNumberChecker.hasTeen(12, 20, 21));
+
+    }
+}
