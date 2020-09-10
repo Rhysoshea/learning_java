b227d974cbb29918aaa975e4eac1d03b9037db00
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-09-10 18:04:32 +0900

    exercise 8

---
+++ expressions_and_more/AreaCalculator/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(AreaCalculator.area(3));
+        System.out.println(AreaCalculator.area(4,5));
+        System.out.println(AreaCalculator.area(4, -1));
+        System.out.println(AreaCalculator.area(4, 0));
+        System.out.println(AreaCalculator.area(5));
+
+    }
+}
