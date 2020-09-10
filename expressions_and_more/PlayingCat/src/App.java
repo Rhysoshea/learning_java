f81734d7ae7638c6d217b188b99b2025ddfd295b
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-09-10 18:05:15 +0900

    exercise 11

---
+++ expressions_and_more/PlayingCat/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(PlayingCat.isCatPlaying(true, 10));
+        System.out.println(PlayingCat.isCatPlaying(false, 36));
+        System.out.println(PlayingCat.isCatPlaying(false, 35));
+
+    }
+}
