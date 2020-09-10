a0a4c48ef90b42931333a096aa4b44b2a482f59f
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-09-10 18:04:48 +0900

    exercise 10

---
+++ expressions_and_more/EqualityPrinter/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        IntEqualityPrinter.printEqual(1, 1, 1);
+        IntEqualityPrinter.printEqual(1, 1, 2);
+        IntEqualityPrinter.printEqual(1, 2, 3);
+        IntEqualityPrinter.printEqual(1, 1, -1);
+
+    }
+}
