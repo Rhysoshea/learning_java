4fdce1da322cb449673b141345cfd19cfb6f96f1
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-09-10 14:39:05 +0900

    exercise 6

---
+++ expressions_and_more/EqualSumChecker/src/App.java
@@ -0,0 +1,10 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(EqualSumChecker.hasEqualSum(1, 1, 2));
+        System.out.println(EqualSumChecker.hasEqualSum(-1, 1, 2));
+        System.out.println(EqualSumChecker.hasEqualSum(-1, 1, 0));
+        System.out.println(EqualSumChecker.hasEqualSum(1, 1, 1));
+
+    }
+}
