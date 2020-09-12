df7d3941c0e8613f532fe7c07b2f71a22441ce04
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-09-12 16:38:11 +0900

    sum odd challenge

---
+++ ControlFlowStatements/SumOdd/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(SumOddRange.sumOdd(1, 1000));
+        System.out.println(SumOddRange.sumOdd(1, 1));
+        System.out.println(SumOddRange.sumOdd(1, 0));
+        System.out.println(SumOddRange.sumOdd(-1, 1000));
+        System.out.println(SumOddRange.sumOdd(-1, -1));
+        System.out.println(SumOddRange.sumOdd(13, 13));
+
+
+    }
+}
