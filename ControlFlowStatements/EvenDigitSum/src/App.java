e13c9c7c9d539bdb2f9ee2c1010d08135874726f
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-09-14 22:38:15 +0900

    exercise 17

---
+++ ControlFlowStatements/EvenDigitSum/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(EvenDigitSum.getEvenDigitSum(0));
+        System.out.println(EvenDigitSum.getEvenDigitSum(252));
+        System.out.println(EvenDigitSum.getEvenDigitSum(-10));
+        System.out.println(EvenDigitSum.getEvenDigitSum(13579));
+        System.out.println(EvenDigitSum.getEvenDigitSum(2468));
+        System.out.println(EvenDigitSum.getEvenDigitSum(123456));
+    }
+}
