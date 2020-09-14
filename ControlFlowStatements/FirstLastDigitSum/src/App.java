461e32ba9c6990e94996d7735d9b48d5cf49072d
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-09-14 22:38:33 +0900

    exercise 16

---
+++ ControlFlowStatements/FirstLastDigitSum/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(252));
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(0));
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(5));
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(-10));
+        System.out.println(FirstLastDigitSum.sumFirstAndLastDigit(11));
+
+    }
+}
