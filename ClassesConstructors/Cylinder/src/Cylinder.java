43c80bcbe3f650bbad92ef9b4bbaf7bd3479cdb9
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-10-06 22:55:45 +0900

    exercise 35

---
+++ ClassesConstructors/Cylinder/src/Cylinder.java
@@ -0,0 +1,23 @@
+public class Cylinder extends Circle {
+    
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+        if (height <0){
+            height=0;
+        }
+        this.height = height;
+    }
+
+
+    public double getHeight() {
+        return this.height;
+    }
+
+
+    public double getVolume(){
+        return getRadius() *getRadius( )*Math.PI*this.height;
+    }
+}
+
