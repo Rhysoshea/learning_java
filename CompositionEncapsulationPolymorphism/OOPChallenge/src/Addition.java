7c5355b15b088166d878c5a8203b627b9b277aee
Rhysoshea <rhys.shea.10@ucl.ac.uk>
2020-10-07 22:54:19 +0900

    OOP challenge

---
+++ CompositionEncapsulationPolymorphism/OOPChallenge/src/Addition.java
@@ -0,0 +1,19 @@
+public class Addition {
+        private String name;
+        private double price;
+
+
+    public Addition(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+
+}
